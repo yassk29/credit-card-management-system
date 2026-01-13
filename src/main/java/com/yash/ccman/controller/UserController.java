34f38b005ab6e697be2fcaa108be863e3fcdadb2
yassk29 <yashkotadiya9008@gmail.com>
2026-01-14 01:18:05 +0530

    feat: add get user apis

--- src/main/java/com/yash/ccman/controller/UserController.java
+++ src/main/java/com/yash/ccman/controller/UserController.java
@@ -4,7 +4,7 @@ import com.yash.ccman.entity.User;
 import com.yash.ccman.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
+import java.util.List;
 @RestController
 @RequestMapping("users")
 @RequiredArgsConstructor
@@ -14,5 +14,15 @@ public class UserController {
     @PostMapping
     public User createUser(@RequestBody User user) {
         return userService.createUser((user));
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
     }
 }
--- src/main/java/com/yash/ccman/service/UserService.java
+++ src/main/java/com/yash/ccman/service/UserService.java
@@ -5,6 +5,8 @@ import com.yash.ccman.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -13,4 +15,12 @@ public class UserService {
     public User createUser(User user) {
         return userRepository.save(user);
     }
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+    public User getUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
 }
+
