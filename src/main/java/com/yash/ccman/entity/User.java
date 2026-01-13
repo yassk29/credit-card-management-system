abe829a4e49709ecd04ee9bd3d070e0b271ade84
yassk29 <yashkotadiya9008@gmail.com>
2026-01-14 00:15:54 +0530

    feat: add user entity and updated user in app properties

---
+++ src/main/java/com/yash/ccman/entity/User.java
@@ -0,0 +1,25 @@
+package com.yash.ccman.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String phoneNumber;
+}
