568087081bec96809187aba6799a33caeb31862b
yassk29 <yashkotadiya9008@gmail.com>
2026-01-13 22:20:58 +0530

    feat: initial spring boot project setup

---
+++ src/main/java/com/yash/ccman/CcManApplication.java
@@ -0,0 +1,13 @@
+package com.yash.ccman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcManApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yash/ccman/CcManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yash.ccman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcManApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
